32e5d9abd36d9ec7c6006c44977ae28ae1cc7e41
woojin2296 <woojin2296@kakao.com>
2023-02-19 13:38:48 +0000

    create project

---
+++ src/main/java/me/delluna/dellunafurniture/DellunaFurniture.java
@@ -0,0 +1,10 @@
+package me.delluna.dellunafurniture;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DellunaFurniture extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+}
